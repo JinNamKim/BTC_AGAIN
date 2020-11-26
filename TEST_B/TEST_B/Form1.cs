c8bb8f58672580e6cb6515dc51c14a38050513c3
JIN <intokyo@naver.com>
2020-11-26 15:21:54 +0900

    COMMIT INIT

---
+++ TEST_B/TEST_B/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
+
+namespace TEST_B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ur = "https://api.binance.com/api/v3/depth?symbol=LTCBTC&limit=5";
+            string retult = callWebClient(ur);
+            Console.WriteLine(retult);
+
+            var json = new JObject();
+
+            var json5 = JObject.Parse(retult);
+            //Console.WriteLine(json5.ToString());
+            Console.WriteLine(json5["bids"]);
+
+            Console.WriteLine(json5["bids"][0]);
+
+            //Console.WriteLine(json.ToString());
+
+
+
+        }
+
+        public static string callWebClient(string targetURL)
+        {
+            string result = string.Empty;
+            try
+            {
+                WebClient client = new WebClient();
+
+                //특정 요청 헤더값을 추가해준다. 
+                client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+                using (Stream data = client.OpenRead(targetURL))
+                {
+                    using (StreamReader reader = new StreamReader(data))
+                    {
+                        string s = reader.ReadToEnd();
+                        result = s;
+
+                        reader.Close();
+                        data.Close();
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                //통신 실패시 처리로직
+                Console.WriteLine(e.ToString());
+            }
+            return result;
+        }
+    }
+}
